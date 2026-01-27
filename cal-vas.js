6519589b6dd40798f2b7c8206a0963fbcbeff304
Calvin Smith <hylthek@gmail.com>
2026-01-27 02:39:29 -0800

    mild refactor

--- cal-vas.js
+++ cal-vas.js
@@ -1,27 +1,9 @@
 import { InitBuffers } from "./init-buffers.js";
 import { DrawScene } from "./draw-scene.js";
+import { InitShaderProgram } from "./init-shader-program.js";
+import { LoadTexture } from "./load-texture.js";
 
-let cubeRotation = 0.0;
-let deltaTime = 0;
-
-/** Returns WebGL context. */
-function CanvasInit() {
-  const canvas = document.getElementById("cal-vas")
-  canvas.width = canvas.clientWidth; // Resolution
-  canvas.height = canvas.clientHeight; // Resolution
-  return canvas.getContext("webgl2")
-}
-
-/** Returns 2D context. */
-function DebugCanvasInit() {
-  const canvas = document.getElementById("debug-canvas")
-  const resolution_factor = 3
-  canvas.width = canvas.clientWidth * resolution_factor; // Resolution * res_factor
-  canvas.height = canvas.clientHeight * resolution_factor; // Resolution * res_factor
-  return canvas.getContext("2d")
-}
-
-// OpenType proof of concept below.
+// OpenType proof of concept via 2d canvas.
 {
   // Get the ttfs and use opentype to parse.
   const jetbrains_mono_url = 'jetbrainsmono_ttf/JetBrainsMonoNL-Regular.ttf';
@@ -57,17 +39,17 @@ function DebugCanvasInit() {
       case 'L':
         debug_ctx.lineTo(command.x, command.y)
         break;
-      case 'Q':
+      case 'Q': // We forgo bezier curves for this proof of concept.
         debug_ctx.lineTo(command.x1, command.y1)
         debug_ctx.lineTo(command.x, command.y)
         break;
-      case 'C':
+      case 'C': // We forgo bezier curves for this proof of concept.
         debug_ctx.lineTo(command.x1, command.y1)
         debug_ctx.lineTo(command.x2, command.y2)
         debug_ctx.lineTo(command.x, command.y)
         break;
       case 'Z':
-        debug_ctx.stroke()
+        debug_ctx.fill()
         break;
       default:
         console.error("Unhandled opentype command: " + command.type)
@@ -78,17 +60,14 @@ function DebugCanvasInit() {
 
 async function RoomMain() {
   const gl = CanvasInit()
-  if (!gl) {
-    console.error("WebGL not supported")
-    return
-  }
+  if (!gl) { console.error("WebGL not supported"); return; }
   gl.clearColor(255, 255, 255, 1.0)
   gl.clear(gl.COLOR_BUFFER_BIT);
 
-  // Build shader.
+  // Build and bind shader.
   const vertex_shader_src = await (await fetch("vertex.glsl")).text()
   const fragment_shader_src = await (await fetch("fragment.glsl")).text()
-  const shaderProgram = initShaderProgram(gl, vertex_shader_src, fragment_shader_src)
+  const shaderProgram = InitShaderProgram(gl, vertex_shader_src, fragment_shader_src)
 
   // Collect all the info needed to use the shader program.
   // Look up which attributes our shader program is using for aVertexPosition, aTextureCoord and also look up uniform locations.
@@ -121,137 +100,29 @@ async function RoomMain() {
   ]
 
   // Draw the scene repeatedly
-  let then = 0;
   function RenderScene(now) {
-    now *= 0.001; // convert to seconds
-    deltaTime = now - then;
-    then = now;
-
-    DrawScene(gl, programInfo, buffers, textures, cubeRotation);
-    cubeRotation += deltaTime;
+    const cube_rotation = now / 1000;
+    DrawScene(gl, programInfo, buffers, textures, cube_rotation);
 
     requestAnimationFrame(RenderScene);
   }
   requestAnimationFrame(RenderScene);
 }
-
-// Initial call.
 RoomMain()
 
-//
-// Initialize a shader program, so WebGL knows how to draw our data
-//
-function initShaderProgram(gl, vsSource, fsSource) {
-  const vertexShader = loadShader(gl, gl.VERTEX_SHADER, vsSource);
-  const fragmentShader = loadShader(gl, gl.FRAGMENT_SHADER, fsSource);
-
-  // Create the shader program
-
-  const shaderProgram = gl.createProgram();
-  gl.attachShader(shaderProgram, vertexShader);
-  gl.attachShader(shaderProgram, fragmentShader);
-  gl.linkProgram(shaderProgram);
-
-  // If creating the shader program failed, alert
-
-  if (!gl.getProgramParameter(shaderProgram, gl.LINK_STATUS)) {
-    alert(`Unable to initialize the shader program: ${gl.getProgramInfoLog(shaderProgram)}`);
-    return null;
-  }
-
-  return shaderProgram;
+/** Returns WebGL context. */
+function CanvasInit() {
+  const canvas = document.getElementById("cal-vas")
+  canvas.width = canvas.clientWidth; // Resolution
+  canvas.height = canvas.clientHeight; // Resolution
+  return canvas.getContext("webgl2")
 }
 
-//
-// creates a shader of the given type, uploads the source and
-// compiles it.
-//
-function loadShader(gl, type, source) {
-  const shader = gl.createShader(type);
-
-  // Send the source to the shader object
-
-  gl.shaderSource(shader, source);
-
-  // Compile the shader program
-
-  gl.compileShader(shader);
-
-  // See if it compiled successfully
-
-  if (!gl.getShaderParameter(shader, gl.COMPILE_STATUS)) {
-    alert(
-      `An error occurred compiling the shaders: ${gl.getShaderInfoLog(shader)}`,
-    );
-    gl.deleteShader(shader);
-    return null;
-  }
-
-  return shader;
-}
-
-//
-// Initialize a texture and load an image.
-// When the image finished loading copy it into the texture.
-//
-function LoadTexture(gl, url) {
-  const texture = gl.createTexture(); // Empty texture.
-  gl.bindTexture(gl.TEXTURE_2D, texture); // Bind empty texture to gl context's current texture.
-
-  // Because images have to be downloaded over the internet they might take a moment until they are ready.
-  // Until then put a single pixel in the texture so we can use it immediately.
-  // When the image has finished downloading we'll update the texture with the contents of the image.
-  const level = 0;
-  const internalFormat = gl.RGBA;
-  const width = 1;
-  const height = 1;
-  const border = 0;
-  const srcFormat = gl.RGBA;
-  const srcType = gl.UNSIGNED_BYTE;
-  const pixel = new Uint8Array([0, 0, 0, 0]); // Transparent black.
-  gl.texImage2D( // This function is a setter.
-    gl.TEXTURE_2D,
-    level,
-    internalFormat,
-    width,
-    height,
-    border,
-    srcFormat,
-    srcType,
-    pixel,
-  );
-
-  const image = new Image();
-  image.onload = () => {
-    gl.bindTexture(gl.TEXTURE_2D, texture);
-    gl.texImage2D(
-      gl.TEXTURE_2D,
-      level,
-      internalFormat,
-      srcFormat,
-      srcType,
-      image,
-    );
-
-    // WebGL1 has different requirements for power of 2 images
-    // vs. non power of 2 images so check if the image is a
-    // power of 2 in both dimensions.
-    if (isPowerOf2(image.width) && isPowerOf2(image.height)) {
-      // Yes, it's a power of 2. Generate mips.
-      gl.generateMipmap(gl.TEXTURE_2D);
-    } else {
-      // No, it's not a power of 2. Turn off mips and set
-      // wrapping to clamp to edge
-      gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE);
-      gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.CLAMP_TO_EDGE);
-      gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.LINEAR);
-    }
-  };
-  image.src = url;
-
-  return texture;
-}
-
-function isPowerOf2(value) {
-  return (value & (value - 1)) === 0;
+/** Returns 2D context. */
+function DebugCanvasInit() {
+  const canvas = document.getElementById("debug-canvas")
+  const resolution_factor = 3
+  canvas.width = canvas.clientWidth * resolution_factor; // Resolution * res_factor
+  canvas.height = canvas.clientHeight * resolution_factor; // Resolution * res_factor
+  return canvas.getContext("2d")
 }
