2f4040ed8dc304f85da44a99c7b27e348a7e8b46
Calvin Smith <hylthek@gmail.com>
2026-01-27 04:03:50 -0800

    renames and comments to show glsl understanding

--- fragment.glsl
+++ fragment.glsl
@@ -1,7 +1,7 @@
-varying highp vec2 vTextureCoord;
+varying highp vec2 _vTextureCoord; // This variable is private and is grabbed directly from the vert shader.
 
 uniform sampler2D uSampler;
 
 void main(void) {
-  gl_FragColor = texture2D(uSampler, vTextureCoord);
+  gl_FragColor = texture2D(uSampler, _vTextureCoord);
 }
--- vertex.glsl
+++ vertex.glsl
@@ -4,9 +4,9 @@ attribute vec2 aTextureCoord; //
 uniform mat4 uModelViewMatrix;
 uniform mat4 uProjectionMatrix;
 
-varying highp vec2 vTextureCoord;
+varying highp vec2 _vTextureCoord; // This variable is private and is passed directly to the frag shader.
 
 void main(void) {
   gl_Position = uProjectionMatrix * uModelViewMatrix * aVertexPosition;
-  vTextureCoord = aTextureCoord;
+  _vTextureCoord = aTextureCoord;
 }
