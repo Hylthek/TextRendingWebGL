f608f4de829492ee34cdfe335220c87cd3831153
Calvin Smith <hylthek@gmail.com>
2026-01-28 15:55:39 -0800

    unimportant

--- vertex.glsl
+++ vertex.glsl
@@ -5,6 +5,7 @@ uniform mat4 uModelViewMatrix;
 uniform mat4 uProjectionMatrix;
 
 varying highp vec2 _vTextureCoord; // This variable is private and is passed directly to the frag shader.
+// Called "varying" because the fragment shader will interpolate the value between vertices.
 
 void main(void) {
   gl_Position = uProjectionMatrix * uModelViewMatrix * aVertexPosition;
